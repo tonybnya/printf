e776e7e85b565f70b343ed9e6a411c86cc311182
Tony <nya.tony2010@gmail.com>
2022-10-19 01:57:25 +0100

    Update: print.c

--- print.c
+++ print.c
@@ -5,12 +5,21 @@
  * @format: string representing the format of arguments
  * @args: variadic arguments
  *
+ * mode:
+ * 0 - simple printing
+ * 1 - escape printing
+ *
  * Return: void
  */
 int print(const char *format, va_list args)
 {
+	void *ptr_num;
 	int result;
+	int i;
+	int is_long = 0;
 	int mode = 0;
+	int reset = 1;
+	char num_buf[65];
 
 	while (*format)
 	{
@@ -44,10 +53,145 @@ int print(const char *format, va_list args)
 					break;
 				}
 
+				case 'd':
+				case 'i':
+				{
+					int64_t num;
+
+					if (is_long == 1)
+					{
+						num = va_arg(args, long);
+					}
+					else
+					{
+						num = va_arg(args, int);
+					}
+
+					num2str(num, 10, num_buf);
+
+					for (i = 0; num_buf[i]; i++)
+					{
+						result += _putchar(num_buf[i]);
+					}
+					break;
+				}
+
+				case 'u':
+				{
+					unsigned int num;
+
+					if (is_long == 1)
+					{
+						num = va_arg(args, long);
+					}
+					else
+					{
+						num = va_arg(args, int);
+					}
+
+					unsigned_num2str(num, 10, num_buf);
+
+					for (i = 0; num_buf[i]; i++)
+					{
+						result += _putchar(num_buf[i]);
+					}
+					break;
+				}
+
+				case 'l':
+				{
+					is_long = 1;
+					reset = 0;
+					break;
+				}
+
+				case 'x':
+				{
+					uint64_t num;
+
+					if (is_long == 1)
+					{
+						num = va_arg(args, unsigned long);
+					}
+					else
+					{
+						num = va_arg(args, unsigned int);
+					}
+
+					unsigned_num2str(num, 16, num_buf);
+
+					for (i = 0; num_buf[i]; i++)
+						result += _putchar(num_buf[i]);
+					break;
+				}
+
+				case 'X':
+				{
+					uint64_t num;
+
+					if (is_long == 1)
+					{
+						num = va_arg(args, unsigned long);
+					}
+					else
+					{
+						num = va_arg(args, unsigned int);
+					}
+
+					unsigned_num2str(num, 16, num_buf);
+
+					for (i = 0; num_buf[i]; i++)
+					{
+						if (isalpha(num_buf[i]))
+						{
+							char c = toupper(num_buf[i]);
+							result += _putchar(c);
+						}
+						else
+						{
+							result += _putchar(num_buf[i]);
+						}
+					}
+					break;
+				}
+
+				case 'p':
+				{
+					result += _putchar('0');
+					result += _putchar('x');
+
+					ptr_num = va_arg(args, void *);
+					unsigned_num2str((uint64_t) ptr_num, 16, num_buf);
+
+					for (i = 0; num_buf[i]; i++)
+						result += _putchar(num_buf[i]);
+					break;
+				}
+
+				case 'f':
+				{
+					break;
+				}
+
+				case 'o':
+				{
+					break;
+				}
+
 				case '%':
 				{
 					result += _putchar('%');
 				}
+			}
+
+			if (reset == 1)
+			{
+				mode = 0;
+				is_long = 0;
+			}
+			else
+			{
+				reset = 1;
 			}
 		}
 
