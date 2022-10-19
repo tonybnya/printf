9e3e9ef2e676414211ca0e0bbafc3d761be3b6c0
Tony <nya.tony2010@gmail.com>
2022-10-19 04:11:22 +0100

    Update header file

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFSIZE 1024
-
 #include <ctype.h>
 #include <limits.h>
 #include <stdarg.h>
@@ -12,22 +10,10 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * struct printer - structure for printing various types
- * @symbol: type to print
- * @func_t: function to print
- */
-typedef struct printer
-{
-	const char symbol;
-	int (*func_t)(va_list);
-} printer_t;
-
+int _printf(const char *format, ...);
 int print(const char *format, va_list args);
-int _write(char c);
-int print_char(va_list arg_list);
-int print_string(va_list arg_list);
-int print_percent(__attribute__((unused))va_list arguments);
-int (*flags(const char c))(va_list);
+int _putchar(char c);
+void unsigned_num2str(uint64_t num, int base, char *buffer);
+void num2str(int64_t num, int base, char *buffer);
 
 #endif /* MAIN_H */
